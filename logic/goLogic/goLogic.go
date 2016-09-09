aebac21c70a300bb02d4845f70fd3722eb77004d
Keita_Nakamura <k.slp.0629@gmail.com>
2016-09-09 16:55:31 +0900

    win path

--- logic/goLogic/goLogic.go
+++ logic/goLogic/goLogic.go
@@ -17,6 +17,6 @@ func (aLogic *GoLogic)Process(*model.Bid, []model.Advertiser) (*logic.Response, 
 
 	// TODO implement
 
-	return &logic.Response{ID:"hoge",BidPrice:6000,AdvertiserID:"adv1",Nurl:"htt://hoge"},nil
+	return &logic.Response{ID:"hoge",BidPrice:6000,AdvertiserID:"adv1",Nurl:"htt://104.199.211.201:80/dsp/dsp/rest/v1/win"},nil
 }
 
