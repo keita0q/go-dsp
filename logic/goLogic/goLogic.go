31c1c2c6a6a55cca77984a25340681cda08b98ba
Keita_Nakamura <k.slp.0629@gmail.com>
2016-09-09 17:12:43 +0900

    sleep

--- logic/goLogic/goLogic.go
+++ logic/goLogic/goLogic.go
@@ -3,6 +3,7 @@ package goLogic
 import (
 	"github.com/keita0q/go-dsp/logic"
 	"github.com/keita0q/go-dsp/model"
+	"time"
 )
 
 type GoLogic struct {
@@ -17,6 +18,7 @@ func (aLogic *GoLogic)Process(*model.Bid, []model.Advertiser) (*logic.Response, 
 
 	// TODO implement
 
-	return &logic.Response{ID:"hoge",BidPrice:6000,AdvertiserID:"adv1",Nurl:"htt://104.199.211.201:80/dsp/dsp/rest/v1/win"},nil
+	time.Sleep(100 * time.Millisecond)
+	return &logic.Response{ID:"hoge", BidPrice:6000, AdvertiserID:"adv1", Nurl:"htt://104.199.211.201:80/dsp/dsp/rest/v1/win"}, nil
 }
 
