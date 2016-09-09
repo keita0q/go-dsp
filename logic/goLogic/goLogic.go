5d4b3d903e1b6a22c4d7e866c1ea83eb7631acc4
Keita_Nakamura <k.slp.0629@gmail.com>
2016-09-09 19:12:32 +0900

    sleep

--- logic/goLogic/goLogic.go
+++ logic/goLogic/goLogic.go
@@ -18,7 +18,7 @@ func (aLogic *GoLogic)Process(*model.Bid, []model.Advertiser) (*logic.Response, 
 
 	// TODO implement
 
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(50 * time.Millisecond)
 	return &logic.Response{ID:"hoge", BidPrice:6000, AdvertiserID:"adv1", Nurl:"htt://104.199.211.201:80/dsp/dsp/rest/v1/win"}, nil
 }
 
